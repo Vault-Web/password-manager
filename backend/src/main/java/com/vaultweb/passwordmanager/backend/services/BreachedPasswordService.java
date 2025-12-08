b64f5bd9906f64d204f08a652a41b683c7df5a8a
Rashmi Soni <rashmi98soni@gmail.com>
2025-12-08 10:42:41 +0530

    adding UTF8 encoding

--- backend/src/main/java/com/vaultweb/passwordmanager/backend/services/BreachedPasswordService.java
+++ backend/src/main/java/com/vaultweb/passwordmanager/backend/services/BreachedPasswordService.java
@@ -1,6 +1,8 @@
 package com.vaultweb.passwordmanager.backend.services;
 
 import com.vaultweb.passwordmanager.backend.exceptions.PasswordBreachCheckException;
+
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -20,7 +22,7 @@ public class BreachedPasswordService {
   public int checkIfBreached(String password) {
     try {
       MessageDigest digest = MessageDigest.getInstance("SHA-1");
-      byte[] hashBytes = digest.digest(password.getBytes());
+      byte[] hashBytes = digest.digest(password.getBytes(StandardCharsets.UTF_8));
       StringBuilder sb = new StringBuilder();
       for (byte b : hashBytes) sb.append(String.format("%02X", b));
       String hash = sb.toString();
