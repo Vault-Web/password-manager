93da885ce3910b4fb3e8f8b3f02a563e31d44138
msrajput08 <msrajput8112004@gmail.com>
2026-01-06 23:50:40 +0530

    style: fix Spotless formatting for auto-config exclusion

--- backend/src/test/java/com/vaultweb/passwordmanager/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/vaultweb/passwordmanager/backend/BackendApplicationTests.java
@@ -4,12 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(
-        properties = {
-            "spring.autoconfigure.exclude=" +
-            "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
-            "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
-        }
-)
+    properties = {
+      "spring.autoconfigure.exclude="
+          + "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,"
+          + "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
+    })
 class BackendApplicationTests {
 
   @Test
